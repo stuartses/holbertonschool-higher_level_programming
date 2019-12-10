70685b981f17415a77aee9d8b2838aff73f0f46a
stuartses <stuart.ses@hotmail.com>
2019-12-10 21:50:23 +0000

    fix last digit sign

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,10 @@ if number < 0:
     new = -number
 else:
     new = number
+
 last_digit = new % 10
+if number < 0:
+    last_digit = -last_digit
 
 if last_digit > 5:
     str = 'and is greater than 5'
