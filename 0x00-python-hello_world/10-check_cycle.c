faad2a700b34e7c2b21b1671d2209b8a3b5ec034
stuartses <stuart.ses@hotmail.com>
2019-12-10 02:21:07 +0000

    update 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
 #include "lists.h"
 /**
  * check_cycle - check if list is a cycle
  * @list: input list
  *
  * Description: check if a list is a cycle, return 1 if is cycle
- * Return: 1 is a cycle, 0 is nos a cycle
+ * Return: 1 is a cycle, 0 is not a cycle
  */
 int check_cycle(listint_t *list)
 {
