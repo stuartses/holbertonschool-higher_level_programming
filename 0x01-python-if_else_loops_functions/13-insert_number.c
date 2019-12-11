b2f3e20a14d279eb9b47e6aab54b4df5f789ed78
stuartses <stuart.ses@hotmail.com>
2019-12-11 01:25:38 +0000

    Change condition if input is NULL

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -14,8 +14,11 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *new, *temp;
 
 	temp = *head;
+	if (temp == NULL)
+		return (NULL);
+
 	new = malloc(sizeof(listint_t));
-	if (new == NULL || temp == NULL)
+	if (new == NULL)
 		return (NULL);
 
 	new->n = number;
