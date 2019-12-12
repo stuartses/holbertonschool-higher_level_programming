80bce21ad5cf7658a8bc38135f69e20f76def042
stuartses <stuart.ses@hotmail.com>
2019-12-12 22:56:00 +0000

    Fix conditional main

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
-import sys
-len_argv = len(sys.argv) - 1
-
-if len_argv == 0:
-    str = "arguments."
-if len_argv == 1:
-    str = "argument:"
-if len_argv > 1:
-    str = "arguments:"
 if __name__ == "__main__":
-    print("{} {}".format(len_argv, str))
+    import sys
+    len_argv = len(sys.argv) - 1
+
+    if len_argv == 0:
+        str = "arguments."
+    if len_argv == 1:
+        str = "argument:"
+    if len_argv > 1:
+        str = "arguments:"
+
     for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
