5a73d5052a7cc3fbc83946937589184576ea0935
stuartses <stuart.ses@hotmail.com>
2019-12-12 23:00:48 +0000

    Fix print

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,5 +10,7 @@ if __name__ == "__main__":
     if len_argv > 1:
         str = "arguments:"
 
+    print("{:d} {:s}".format(len_argv, str))
+
     for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+        print("{:d}: {:s}".format(i, sys.argv[i]))
