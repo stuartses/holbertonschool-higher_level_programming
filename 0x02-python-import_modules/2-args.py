8ddef35e0c0747a2857728138961edf02f2f389f
stuartses <stuart.ses@hotmail.com>
2019-12-12 23:06:42 +0000

    Quit variables

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python
 if __name__ == "__main__":
     import sys
-    len_argv = len(sys.argv) - 1
 
-    if len_argv == 0:
-        str = "arguments."
-    if len_argv == 1:
-        str = "argument:"
-    if len_argv > 1:
-        str = "arguments:"
-
-    print("{:d} {:s}".format(len_argv, str))
+    if len(sys.argv) == 1:
+        print("{:d} {:s}".format(len(sys.argv) - 1, "arguments."))
+    if len(sys.argv) == 2:
+        print("{:d} {:s}".format(len(sys.argv) - 1, "argument:"))
+    if len(sys.argv) > 2:
+        print("{:d} {:s}".format(len(sys.argv) - 1, "arguments:"))
 
     for i in range(1, len(sys.argv)):
         print("{:d}: {:s}".format(i, sys.argv[i]))
