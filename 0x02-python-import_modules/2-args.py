c9949fae768cc3bf782dc0d3837876929c83cb94
stuartses <stuart.ses@hotmail.com>
2019-12-12 23:30:31 +0000

    Fix first line

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,16 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+    len_argv = len(sys.argv) - 1
 
-    if len(sys.argv) == 1:
-        print("{:d} {:s}".format(len(sys.argv) - 1, "arguments."))
-    if len(sys.argv) == 2:
-        print("{:d} {:s}".format(len(sys.argv) - 1, "argument:"))
-    if len(sys.argv) > 2:
-        print("{:d} {:s}".format(len(sys.argv) - 1, "arguments:"))
+    if len_argv == 0:
+        str = "arguments."
+    if len_argv == 1:
+        str = "argument:"
+    if len_argv > 1:
+        str = "arguments:"
+
+    print("{:d} {:s}".format(len_argv, str))
 
     for i in range(1, len(sys.argv)):
         print("{:d}: {:s}".format(i, sys.argv[i]))
