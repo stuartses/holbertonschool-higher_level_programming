ee203cfb9e5835193d1eae6d74d03f9b00b12cdb
stuartses <stuart.ses@hotmail.com>
2019-12-19 22:44:17 +0000

    Fix sorted paramter

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    a_sorted = sorted(a_dictionary)
+    a_sorted = sorted(a_dictionary.keys())
     for i in a_dictionary:
         print("{:s}: {}".format(i, a_dictionary[i]))
