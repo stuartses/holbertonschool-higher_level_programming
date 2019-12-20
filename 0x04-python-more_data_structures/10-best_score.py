3d47ee0abb0d150772683fe26b8ab2660b6e0eb4
stuartses <stuart.ses@hotmail.com>
2019-12-20 00:00:52 +0000

    Change conditions

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,9 +4,6 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
 
-    if len(a_dictionary) == 0:
-        return None
-
     for i in a_dictionary:
         if a_dictionary[i] > best:
             best = a_dictionary[i]
