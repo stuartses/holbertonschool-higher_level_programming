515b1b23af1e5c786c929a9b9b717de5be69b667
stuartses <stuart.ses@hotmail.com>
2020-01-08 21:01:56 +0000

    Update Task 3. Inside result and Finally

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    res = 0
     try:
-        return (a / b)
+        res = a / b
     except ZeroDivisionError:
-        return None
+        res = None
+    finally:
+        print("Inside result: {}".format(res))
+
+    return res
