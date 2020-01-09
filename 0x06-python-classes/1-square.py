14ff3b4e752cbf9d791466628d62b9ca38d0658e
stuartses <stuart.ses@hotmail.com>
2020-01-09 23:06:44 +0000

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """Create a private variable Size.
+
+    Args:
+        size: size of square
+    """
+
+    def __init__(self, size):
+        self.__size = size
