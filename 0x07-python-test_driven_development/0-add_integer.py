d2a41ea3437f12116ef551d00b062b59b1782691
stuartses <stuart.ses@hotmail.com>
2020-01-12 22:04:07 +0000

    Add one test case more, when arguments is empty

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -21,8 +21,10 @@ def add_integer(a, b=98):
         int: The return value convert arguments to integer and adds.
 
     """
+
     if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
+
     if type(b) != int and type(b) != float:
         raise TypeError("b must be an integer")
 
