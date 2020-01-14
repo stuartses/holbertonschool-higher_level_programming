47799e347b61f5951face68d5eac3e369968110d
Stuart Echeverry <stuart.ses@hotmail.com>
2020-01-14 14:25:41 -0500

    Fix PEP8

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
-""" 0. Simple rectangle 
+""" 0. Simple rectangle
 This module creates a empty class Rectangle.
 Corresponds to Task 0.
 Holberton School
 Foundations - Higher-level programming - Python
 By Stuart Echeverry
 """
+
 
 class Rectangle:
     """Empty Class Rectangle"""
