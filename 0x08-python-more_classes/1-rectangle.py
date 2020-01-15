4b98f9c88f0f8aeb9fb515124fbd030a2db67a1b
stuartses <stuart.ses@hotmail.com>
2020-01-15 15:01:49 +0000

    fix when args are less than zero

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -26,6 +26,12 @@ class Rectangle:
             height: height of rectangle
         """
 
+        if width < 0:
+            raise ValueError("width must be >= 0")
+
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
         self.__width = width
         self.__height = height
 
