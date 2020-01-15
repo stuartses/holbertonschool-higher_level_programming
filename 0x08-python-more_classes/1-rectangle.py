1885a558b42409521c4b6f1be61d571459ea8719
stuartses <stuart.ses@hotmail.com>
2020-01-15 15:06:11 +0000

    Fix type filter by input

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -26,6 +26,12 @@ class Rectangle:
             height: height of rectangle
         """
 
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+
         if width < 0:
             raise ValueError("width must be >= 0")
 
