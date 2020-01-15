7736bd805564838a2eb56e37290985464df204de
stuartses <stuart.ses@hotmail.com>
2020-01-15 15:15:09 +0000

    Fix input conditions

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -30,6 +30,18 @@ class Rectangle:
             height: height of rectangle
         """
 
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+
+        if width < 0:
+            raise ValueError("width must be >= 0")
+
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
         self.__width = width
         self.__height = height
         type(self).number_of_instances += 1
