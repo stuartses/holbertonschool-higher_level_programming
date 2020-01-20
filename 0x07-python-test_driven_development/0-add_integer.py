3240428138fb7da32a5f8a59ad50bf7293efe1d9
Stuart Echeverry <stuart.ses@hotmail.com>
2020-01-20 10:12:02 -0500

    Correct test cases for Nan and Inf

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -23,16 +23,10 @@ def add_integer(a, b=98):
 
     """
 
-
-    if (type(a) != int and type(a) != float) or a == float('nan'):
+    if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
 
-    if (type(b) != int and type(b) != float) or b == float('nan'):
+    if type(b) != int and type(b) != float:
         raise TypeError("b must be an integer")
-
-    result = a + b
-
-    if result == float('inf') or result == -float('inf'):
-        return 89
 
     return int(a) + int(b)
