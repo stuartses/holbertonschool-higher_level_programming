1b55105f15fca5a7561b29120fd4b376bcb5c994
stuartses <stuart.ses@hotmail.com>
2020-01-21 02:10:48 +0000

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""6. Improve Geometry
+Raise a Error when method area is used
+Corresponds to Task 6.
+Holberton School
+Foundations - Higher-level programming - Python
+By Stuart Echeverry
+"""
+
+
+class BaseGeometry:
+    def area(self):
+        """Defines area"""
+
+        raise Exception("area() is not implemented")
