1c394abac336ce1faba1a548e525e01518642399
stuartses <stuart.ses@hotmail.com>
2020-01-21 01:20:25 +0000

    Task 2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,3 +1,24 @@
 #!/usr/bin/python3
+""" 2. Exact same object
+Check if object is a exact instant of class
+Corresponds to Task 2.
+Holberton School
+Foundations - Higher-level programming - Python
+By Stuart Echeverry
+"""
+
+
 def is_same_class(obj, a_class):
-    return isinstance(obj, a_class)
+    """function that returns True if the object is exactly an instanc
+    of the specified class
+
+    Args:
+        obj: object
+        a_class: class
+
+    Return:
+        True or False
+    """
+
+    is_type = type(obj) == a_class
+    return is_type
