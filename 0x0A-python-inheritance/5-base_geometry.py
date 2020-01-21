12e923a08933edfa37a70aa1878de0ac93e178bb
stuartses <stuart.ses@hotmail.com>
2020-01-21 02:04:08 +0000

    Task 5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""5. Geometry module
+Empty Class
+Corresponds to Task 5.
+Holberton School
+Foundations - Higher-level programming - Python
+By Stuart Echeverry
+"""
+
+
+class BaseGeometry:
+    pass
