a5d2bd3835c07b6ab13625c0514ad616c5517a85
stuartses <stuart.ses@hotmail.com>
2020-03-02 15:24:18 -0500

    Change ORDER by DESC

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- List by best
 -- Query that list by score and name, order by score
-SELECT score, name FROM second_table ORDER BY score;
+SELECT score, name FROM second_table ORDER BY score DESC;
