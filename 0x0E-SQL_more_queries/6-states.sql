17b2255c7e799278f09a0c3283a4fccc9b9b0dfc
stuartses <stuart.ses@hotmail.com>
2020-03-03 18:43:07 -0500

    Create database with a table that has a primary kay and auto increment

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- Create Database and Table
+-- Creates an database and table in  with a primary key auto increment
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY (id));
