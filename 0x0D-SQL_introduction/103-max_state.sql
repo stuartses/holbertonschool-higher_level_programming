a2562ca5e792ea6006df333931c734cb234e7173
stuartses <stuart.ses@hotmail.com>
2020-03-02 19:08:11 -0500

    Quit import line

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,5 +1,3 @@
 -- Temperatures #2
--- Import from sql file
-source temperatures.sql;
 -- List The MAX temperatura GROUP By State
 SELECT state, MAX(value) AS 'max_temp' FROM temperatures GROUP BY state;
