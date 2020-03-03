3e5c6b07ce48d2b31990d52ea6cab81cf2135e82
Stuart Echeverry <stuart.ses@hotmail.com>
2020-03-03 13:50:56 -0500

    Put password for new user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Create a Root User
 -- Query that creates a user and GRANTS all privileges
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
