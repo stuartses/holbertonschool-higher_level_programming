9c84d8c49313fb8666b5c54566f2f67c0b87f7eb
stuartses <stuart.ses@hotmail.com>
2020-03-02 19:06:55 -0500

    quit import line

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,3 @@
 -- Temperatures #0
--- Import from sql file
-source temperatures.sql;
 -- List Average Temperatures order by temperature and city
 SELECT city, AVG(value) AS "avg_temp" FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
