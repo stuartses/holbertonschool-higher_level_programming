aa9eade2c7d5e69bacc2436e1f45ee5b2195b257
Stuart Echeverry <stuart.ses@hotmail.com>
2020-03-03 13:48:14 -0500

    Creates a user with all privileges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Create a Root User
+-- Query that creates a user and GRANTS all privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
