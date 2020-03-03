2d0001bd433c9b80354f16f7b40755e3bd6a35a9
Stuart Echeverry <stuart.ses@hotmail.com>
2020-03-03 13:29:20 -0500

    Delete comment at line 4

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
 -- Show privileges
--- Show privileges for user_0d_1
+-- Show privileges for user_0d_1 and user_0d_2
 SHOW GRANTS FOR user_0d_1@localhost;
--- Show privileges for user_0d_2
 SHOW GRANTS FOR user_0d_2@localhost;
