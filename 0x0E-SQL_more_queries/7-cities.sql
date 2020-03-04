4fa5cc0fbb1dacca6839efd28af9152123ccdf52
stuartses <stuart.ses@hotmail.com>
2020-03-03 20:08:05 -0500

    Create tabe with a foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,4 @@
+-- Create Database and Table
+-- Creates an database and table in  with a foreign key
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT NOT NULL AUTO_INCREMENT, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, PRIMARY KEY (id), FOREIGN KEY (state_id) REFERENCES states(id));
