020fb5e6758c1ae51f6a42f305cac2c02bc7b228
stuartses <stuart.ses@hotmail.com>
2020-03-03 22:57:14 -0500

    Change field tv_genre.name AS genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,3 +1,3 @@
 -- Retrieves count of genres found with tv shows
 -- List the genres that has show and counts
-SELECT tv_genres.name, COUNT(tv_show_genres.genre_id) AS 'number_of_shows' FROM tv_genres, tv_show_genres WHERE tv_genres.id = tv_show_genres.genre_id GROUP BY tv_genres.id ORDER BY number_of_shows DESC;
+SELECT tv_genres.name AS 'genre', COUNT(tv_show_genres.genre_id) AS 'number_of_shows' FROM tv_genres, tv_show_genres WHERE tv_genres.id = tv_show_genres.genre_id GROUP BY tv_genres.id ORDER BY number_of_shows DESC;
