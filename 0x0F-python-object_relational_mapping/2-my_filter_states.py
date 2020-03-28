a87944ac6c5c5553ced84317abaf8a60e3f446f1
stuartses <stuart.ses@hotmail.com>
2020-03-28 15:45:26 -0500

    Find by state name

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""2. Filter states by user input
+
+This module make a MySQL Query using MySQLdb
+Filter states with a input name
+Holberton School
+Foundations - Higher-level programming - Python
+By Stuart Echeverry
+
+"""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+    len_args = len(sys.argv) - 1
+    args = sys.argv
+
+    db = MySQLdb.connect(host='localhost', user=args[1], passwd=args[2],
+                         db=args[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name='{}'"
+                " ORDER BY id ASC".format(args[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
