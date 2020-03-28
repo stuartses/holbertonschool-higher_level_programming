a672f2c73a4e41e425845f317124f8db99732981
stuartses <stuart.ses@hotmail.com>
2020-03-28 15:21:45 -0500

    Fix breaking line in long string

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -18,7 +18,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', user=args[1], passwd=args[2],
                          db=args[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' "
+                "ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
