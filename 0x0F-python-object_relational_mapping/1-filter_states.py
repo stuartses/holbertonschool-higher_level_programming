2c467a6129a8501a4f8f6be8c1f3a3d144554407
stuartses <stuart.ses@hotmail.com>
2020-03-28 12:11:29 -0500

    filter with a name starting with N (upper N)

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""1. Get all states. Filter states
+
+This module make a MySQL Query using MySQLdb
+Filter states with a name starting with N (upper N)
+Holberton School
+Foundations - Higher-level programming - Python
+By Stuart Echeverry
+
+"""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+    len_argv = len(sys.argv) - 1
+    args = sys.argv
+
+    db = MySQLdb.connect(host='localhost', user=args[1], passwd=args[2],
+                         db=args[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
