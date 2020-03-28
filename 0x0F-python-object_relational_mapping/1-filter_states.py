695b8a9416fd83b63cf0393bde35f8839310d7cf
stuartses <stuart.ses@hotmail.com>
2020-03-28 15:17:48 -0500

    quit conditional by number of arguments, and add LIKE BINARY comparation

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,13 +15,10 @@ if __name__ == "__main__":
     len_args = len(sys.argv) - 1
     args = sys.argv
 
-    if len_args < 3:
-        exit()
-
     db = MySQLdb.connect(host='localhost', user=args[1], passwd=args[2],
                          db=args[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
