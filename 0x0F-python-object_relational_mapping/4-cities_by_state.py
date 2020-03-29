b285b91458af6bbebca79aa7e17f4fa1f3669d51
stuartses <stuart.ses@hotmail.com>
2020-03-29 09:25:52 -0500

    Add JOIN with state table

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -19,7 +19,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', user=args[1], passwd=args[2],
                          db=args[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM cities ORDER BY id ASC")
+    cur.execute("SELECT cities.id, cities.name, states.name "
+                "FROM cities JOIN states ON cities.state_id = states.id "
+                "ORDER BY cities.id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
