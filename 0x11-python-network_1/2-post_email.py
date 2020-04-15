e2bf8f42777a8014c9f8d79844deb74979ce5d2e
stuartses <stuart.ses@hotmail.com>
2020-04-15 13:04:01 -0500

    Send a POST request

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+Send a POST request
+"""
+
+from sys import argv
+import urllib.parse
+import urllib.request
+
+
+url = argv[1]
+email = argv[2]
+values = {'email': email}
+
+# codification
+data = urllib.parse.urlencode(values)
+data = data.encode('ascii')
+
+# request
+req = urllib.request.Request(url, data)
+
+with urllib.request.urlopen(req) as response:
+    content = response.read()
+
+print(content.decode('utf-8'))
