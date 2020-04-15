d87cf5a13b85e5011fc2ee1d106b308396ef94d1
stuartses <stuart.ses@hotmail.com>
2020-04-15 11:46:34 -0500

    fetch url and get content

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Fetch and url and get content
+"""
+
+import urllib.request
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+
+print("\t- type: {}".format(type(html)))
+print("\t- content: {}".format(html))
+print("\t- utf8 content: {}".format(html.decode('utf-8')))
