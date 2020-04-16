5c4b9ecab1bb8e7bdaad756f9f9bbaa33d6a14a1
stuartses <stuart.ses@hotmail.com>
2020-04-15 21:19:34 -0500

    Check if run as the main

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,19 +8,19 @@ from sys import argv
 import urllib.parse
 import urllib.request
 
+if __name__ == "__main__":
+    url = argv[1]
+    email = argv[2]
+    values = {'email': email}
 
-url = argv[1]
-email = argv[2]
-values = {'email': email}
+    # codification
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
 
-# codification
-data = urllib.parse.urlencode(values)
-data = data.encode('ascii')
+    # request
+    req = urllib.request.Request(url, data)
 
-# request
-req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        content = response.read()
 
-with urllib.request.urlopen(req) as response:
-    content = response.read()
-
-print(content.decode('utf-8'))
+        print(content.decode('utf-8'))
