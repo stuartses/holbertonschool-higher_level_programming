1d93d0ef703eb1026a8b983df04265f6554a9bfb
stuartses <stuart.ses@hotmail.com>
2020-04-15 21:48:43 -0500

    Check if program is called by main

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,8 +5,10 @@
 import requests
 from sys import argv
 
-r = requests.get(argv[1])
-if r.ok:  # check if status is between 200 and 400
-    print(r.text)
-else:
-    print("Error code: {}".format(r.status_code))
+
+if __name__ == '__main__':
+    r = requests.get(argv[1])
+    if r.ok:  # check if status is between 200 and 400
+        print(r.text)
+    else:
+        print("Error code: {}".format(r.status_code))
