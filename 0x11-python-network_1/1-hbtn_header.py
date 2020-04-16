44c4ff5e9cb02bac4a52660084656a1c2bef9c57
stuartses <stuart.ses@hotmail.com>
2020-04-15 21:16:21 -0500

    Add __name__ check

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,8 +7,9 @@ Get X-Request-Id from Header in HTTP request
 import urllib.request
 from sys import argv
 
-req = urllib.request.Request(argv[1])
-with urllib.request.urlopen(req) as response:
-    the_headers = response.info()
+if __name__ == "__main__":
+    req = urllib.request.Request(argv[1])
+    with urllib.request.urlopen(req) as response:
+        the_headers = response.info()
 
-print(the_headers.get('X-Request-Id'))
+    print(the_headers.get('X-Request-Id'))
