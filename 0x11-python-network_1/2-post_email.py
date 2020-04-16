df8614fe739c9facbd4c25b43e82dc0dda78d0ad
stuartses <stuart.ses@hotmail.com>
2020-04-15 21:36:52 -0500

    Extrem

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
-
 """
-Send a POST request
+Task 2-Send a POST request to the passed URL with email as a parameter and
+display the body of the response
 """
 
-from sys import argv
-import urllib.parse
-import urllib.request
+if __name__ == '__main__':
+    import urllib.parse
+    import urllib.request
+    from sys import argv
 
-if __name__ == "__main__":
     url = argv[1]
     values = {'email': argv[2]}
 
-    # codification
-    data = urllib.parse.urlencode(values)
-    data = data.encode('ascii')
-
-    # request
-    req = urllib.request.Request(url, data)
-
-    with urllib.request.urlopen(req) as response:
-        content = response.read()
-        print(content.decode('utf-8'))
+    dato = urllib.parse.urlencode(values)
+    dato = dato.encode('ascii')
+    reqst = urllib.request.Request(url, dato)
+    with urllib.request.urlopen(reqst) as response:
+        body = response.read()
+    print(body.decode('utf-8'))
