88ffef257207ee65ebb41adfd199994cac87f510
stuartses <stuart.ses@hotmail.com>
2020-04-16 10:43:14 -0500

    Restore original and use get() method to take data

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
+
 """
-Task 10 - Use the Github API to display a user's ID
+Access to GitHub Api using requests
+Basic Authenication and personal access token
+Is needed a token generated in github with read:user permission (only)
 """
 
+from sys import argv
+import requests
+from requests.auth import HTTPBasicAuth
+
 if __name__ == '__main__':
-    import requests
-    from sys import argv
-    req = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
+    req = requests.get('https://api.github.com/user',
+                       auth=HTTPBasicAuth(argv[1], argv[2]))
+
     print(req.json().get('id'))
