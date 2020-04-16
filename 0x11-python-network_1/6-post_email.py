61b77adaf41867a53d5b60b164898a7c5a8b2aa8
stuartses <stuart.ses@hotmail.com>
2020-04-15 20:06:18 -0500

    POST with requests package

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""POST Request using request package"""
+
+from sys import argv
+import requests
+
+url = argv[1]
+email = argv[2]
+data = {'email': email}
+
+r = requests.post(url, data)
+print(r.text)
