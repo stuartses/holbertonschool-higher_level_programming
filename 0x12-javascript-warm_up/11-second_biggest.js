8d4bd7aabd64172d8621b334db34770d1cb92c3a
stuartses <stuart.ses@hotmail.com>
2020-05-05 16:47:00 -0500

    add Else statement

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
 if (process.argv.length <= 3) console.log(0);
-const sortArgv = process.argv.slice(2).sort().reverse();
-console.log(sortArgv[1]);
+else {
+  const sortArgv = process.argv.slice(2).sort().reverse();
+  console.log(sortArgv[1]);
+}
