0b788f3e4744dcb1d5e9589e224fd7b206f49cb8
stuartses <stuart.ses@hotmail.com>
2020-05-05 18:02:10 -0500

    Implements a compare function with sort

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
 if (process.argv.length <= 3) console.log(0);
 else {
-  const sortArgv = process.argv.slice(2).sort().reverse();
-  console.log(sortArgv[1]);
+  /*
+    implements compare function (a, b) to allow a right sort
+    with negative and cero integers
+  */
+  const sortArgv = process.argv.slice(2).sort(function (a, b) {
+    return a - b;
+  });
+  console.log(sortArgv[sortArgv.length - 2]);
 }
