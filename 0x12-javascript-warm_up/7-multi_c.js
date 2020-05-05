79117892cd8b895bd74d34fd0f2c03ac65c7dd49
stuartses <stuart.ses@hotmail.com>
2020-05-05 16:07:14 -0500

    Without else statement

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 const x = Number(process.argv[2]);
-if (isNaN(x)) {
-  console.log('Missing number of occurrences');
-} else {
-  let i;
-  for (i = 0; i < x; i++) {
-    console.log('C is fun');
-  }
+let i;
+if (isNaN(x)) console.log('Missing number of occurrences');
+for (i = 0; i < x; i++) {
+  console.log('C is fun');
 }
