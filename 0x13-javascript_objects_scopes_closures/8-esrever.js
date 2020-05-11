e7b07946039a358d17d2b41e4cd82830f94db341
stuartses <stuart.ses@hotmail.com>
2020-05-11 18:11:29 -0500

    Fixing style

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,8 +2,8 @@
 
 // returns reversed version of a list
 exports.esrever = function (list) {
-  listReversed = [];
+  const listReversed = [];
   for (let i = list.length - 1; i >= 0; i--) listReversed.push(list[i]);
 
-  return listReversed
+  return listReversed;
 };
