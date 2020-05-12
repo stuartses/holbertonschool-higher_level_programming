730ca6053ef2e1e2c577f2add4e4a219ffc3382f
stuartses <stuart.ses@hotmail.com>
2020-05-12 10:54:38 -0500

    Read content of a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Include fs module
+var fs = require('fs');
+
+// Use fs.readFile() method to read the file
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  // Display the file content
+  if (err) console.error(err);
+  else console.log(data);
+});
