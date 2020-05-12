53fabedd12081fcf563b96703c3859abc7e65117
stuartses <stuart.ses@hotmail.com>
2020-05-12 15:56:51 -0500

    using request.get

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,15 @@
 /* Get the status code of a http request */
 
 const request = require('request');
+/*
 request(process.argv[2], function (error, response) {
   console.log('error:', error);
   console.log('code:', response && response.statusCode);
 });
+*/
+
+request
+  .get(process.argv[2])
+  .on('response', function (response) {
+    console.log('code:', response.statusCode);
+  });
