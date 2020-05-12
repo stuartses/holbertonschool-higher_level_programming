a40a82f7a0cab9084b35b6ac2c3c7afb1e23f7dd
stuartses <stuart.ses@hotmail.com>
2020-05-12 18:27:06 -0500

    Get number of Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+/* Get request and count the character */
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, res, body) {
+  if (err) console.log(err);
+  const results = JSON.parse(body).results;
+  let count = 0;
+
+  for (let i = 0; i < results.length; i++) {
+    const characters = results[i].characters;
+    for (let j = 0; j < characters.length; j++) {
+      const charac = characters[j].split('/');
+      const id = charac[charac.length - 2];
+      if (id === 18) count++;
+    }
+  }
+  console.log(count);
+});
