9404428f384cb466ab68a2465f74268ac35bfcc5
stuartses <stuart.ses@hotmail.com>
2020-05-12 17:47:56 -0500

    quit comments with other code

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,14 +4,6 @@
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-/*
-request
-  .get(url)
-  .on('response', function (response) {
-    console.log(JSON.parse(response));
-  });
-*/
-
 request(url, function (err, res, body) {
   if (err) console.log(err);
   console.log(JSON.parse(body).title);
