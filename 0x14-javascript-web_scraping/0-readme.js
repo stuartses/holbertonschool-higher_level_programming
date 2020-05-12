f67dca435632083f4e0d929e9857a91adcfef67d
stuartses <stuart.ses@hotmail.com>
2020-05-12 14:33:37 -0500

    Using process.stdout

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,6 +5,6 @@ var fs = require('fs');
 // Use fs.readFile() method to read the file
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
   // Display the file content
-  if (err) console.log(err);
-  else console.log(data);
+  if (err) console.error(err);
+  else process.stdout.write(data);
 });
