412fee77d3982777488fd7ae49e666e6580c8e91
stuartses <stuart.ses@hotmail.com>
2020-05-14 11:20:40 -0500

    Fetch and change text

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,9 @@
+// fetch a url and insert value in DIV#character
+
+$.ajax({
+  type: 'GET',
+  url: 'https://swapi-api.hbtn.io/api/people/5/?format=json',
+  success: function (response) {
+    $('#character').text(response.name);
+  }
+});
